8fc0c730be47cfea4a84862a194eb8eb380874c6
Victor Cordeiro Simão <victorcordeiro@discente.ufg.br>
2025-04-05 00:06:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,12 +8,12 @@ let currentProcedimentos = 1;
 async function loadCSVData() {
     try {
         // Carregar dados de procedimentos
-        const procedimentosResponse = await fetch('https://github.com/victorcords/apac-sus/blob/78a11aae2389bd397fb30fc7d183284cbe21ca8f/data/procedimentos.csv');
+        const procedimentosResponse = await fetch('data/procedimentos.csv');
         const procedimentosText = await procedimentosResponse.text();
         procedimentosData = Papa.parse(procedimentosText, { header: true }).data;
         
         // Carregar dados de CID
-        const cidResponse = await fetch('https://github.com/victorcords/apac-sus/blob/78a11aae2389bd397fb30fc7d183284cbe21ca8f/data/cid.csv');
+        const cidResponse = await fetch('data/cid.csv');
         const cidText = await cidResponse.text();
         cidData = Papa.parse(cidText, { header: true }).data;
         
@@ -327,7 +327,7 @@ async function gerarPDF() {
     
     try {
         // 1. Primeiro carregamos o PDF
-        const modelUrl = 'https://github.com/victorcords/apac-sus/blob/78a11aae2389bd397fb30fc7d183284cbe21ca8f/data/modelo.pdf';
+        const modelUrl = 'data/modelo.pdf';
         const existingPdfBytes = await fetch(modelUrl).then(res => res.arrayBuffer());
         
         // 2. Inicializamos o documento PDF
